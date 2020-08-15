0296875757137dfc5bf6a54dee33c489b27eca71
Vojtěch Musílek <vojtechmusilekk@gmail.com>
2020-08-15 14:32:07 +0200

    Removed installer

--- DataZipper/Form1.cs
+++ DataZipper/Form1.cs
@@ -16,5 +16,10 @@ namespace DataZipper
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
